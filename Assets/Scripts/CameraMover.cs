7337f581d3e6d548835366eaccfa7574dc143e60
Recful <59626497+Recful@users.noreply.github.com>
2024-02-25 16:45:54 +0000

    memo move with player + timebar adjustment
    memo move with player + timebar adjustment

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
-    private GameObject player;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +16,6 @@ public class CameraMover : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
     }
 }
--- Assets/Scripts/MemoManager.cs
+++ Assets/Scripts/MemoManager.cs
@@ -10,6 +10,8 @@ public class MemoManager : MonoBehaviour
     [SerializeField] string letter;
     [SerializeField] GameObject letterUI = null;
     [SerializeField] float alpha = 0f;
+
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +21,15 @@ public class MemoManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        GameObject memo = GameObject.FindGameObjectWithTag("Memo");
+        player = GameObject.FindWithTag("Player");
+        Vector2 playerPos = player.transform.position;
+        memo.transform.GetChild(0).position = new Vector2(playerPos.x, playerPos.y);
         if (!UiElement.activeSelf)
         {
             if (Input.GetKeyDown(KeyCode.Tab))
             {
                 UiElement.SetActive(true);
-                GameObject memo = GameObject.FindGameObjectWithTag("Memo");
                 memo.transform.GetChild(0).gameObject.SetActive(true);
                 List<string> tempList = GameManager.Instance.GetItems();
                 for (int i = 0; i < tempList.Count; i++){
