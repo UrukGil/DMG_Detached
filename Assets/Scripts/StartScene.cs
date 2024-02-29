91b2440b6c86a638f0505c19a9c12275117b32a5
Recful <59626497+Recful@users.noreply.github.com>
2024-02-29 16:06:17 +0000

    start game scene + endgame scene
    game end clear playeritems

---
+++ Assets/Scripts/StartScene.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+    // 这个变量用于指向你想要关闭的对象
+    public GameObject gameStartUI;
+
+    // 这个方法会被绑定到按钮的点击事件上
+    public void StartGame()
+    {
+        SceneManager.LoadScene("LivingRoom");
+    }
+}
