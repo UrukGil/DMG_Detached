3f6ad8cfd27080a8fe0472d512c86f5ebb399edd
Finn <160255656+finnpengoxo@users.noreply.github.com>
2024-03-02 14:25:36 +0000

    Update GameOver.cs

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -8,7 +8,7 @@ public class GameOver : MonoBehaviour
     // 这个方法会被绑定到按钮的点击事件上
     public void BackToStartScene()
     {
-        GameManager.Instance.playerItems.Clear();
+        GameManager.Instance.clear();
         SceneManager.LoadScene("StartScene");
     }
 }
