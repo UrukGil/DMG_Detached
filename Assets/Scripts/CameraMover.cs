513980d9b01337e853a98b000d0bec1057e2ee9f
Finn <160255656+finnpengoxo@users.noreply.github.com>
2024-03-15 13:13:03 +0000

    Update for level 3 Street

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMover : MonoBehaviour
 {
     public GameObject player;
+    [SerializeField] float yBias = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,7 @@ public class CameraMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + yBias, transform.position.z);
 
     }
 }
